917a71ba9d5826767316dd41660d801a03819543
kurisubrooks <kurisubrooks@gmail.com>
2016-05-30 21:29:40 +1000

    Launcher/Popovers now fade in/out smoothly

--- js/core.js
+++ js/core.js
@@ -17,10 +17,10 @@ $(document).ready(function() {
     // App Launcher
     $("#app_launcher").click(function() {
         if (popovers.apps) {
-            $(".launcher").fadeOut(20);
+            $(".launcher").fadeOut(25);
             popovers.apps = false;
         } else if (!popovers.apps) {
-            $(".launcher").fadeIn(10);
+            $(".launcher").fadeIn(50);
             popovers.apps = true;
         }
     });
@@ -28,10 +28,10 @@ $(document).ready(function() {
     // Metabar Popover
     $("#meta_launcher").click(function() {
         if (popovers.meta) {
-            $(".metabar-popover").fadeOut(20);
+            $(".metabar-popover").fadeOut(25);
             popovers.meta = false;
         } else if (!popovers.meta) {
-            $(".metabar-popover").fadeIn(10);
+            $(".metabar-popover").fadeIn(50);
             popovers.meta = true;
         }
     });
@@ -55,8 +55,8 @@ $(window).load(function() {
         /*new OS().Alert({
             //title: "地震警報",
             //message: "緊急地震速報<br>岩手県で地震発生。強い揺れに備えて下さい（気象庁）"
-            title: "NSW Emergency Alert",
-            message: "Strong Winds and Torrential Flooding is expected for your area within the next 24 hours. Evacuate to higher ground if possible. (Bureau of Meterology)"
+            title: "System Error",
+            message: "Error occured in the main process. Check the JavaScript console for more details."
         });*/
     }, 1000);
 });
